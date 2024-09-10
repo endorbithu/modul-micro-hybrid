e5410390a22160991d1e831679478347d0ac841f
Magyar Bertalan Márton <102574817+endorbithu@users.noreply.github.com>
2024-09-10 11:08:12 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([\ModuleMicroHybrid\Http\Middleware\ServiceApiIpValidator::class])->group(function () {
-    Route::any('/apiservice/{serviceName}', function ($serviceName) {
-        return response()->json(\ModuleMicroHybrid\Api::resolveIncoming($serviceName, (json_decode(request()->getContent(), true) ?? [])))->setEncodingOptions(JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);;
-    })->where('serviceName', '[a-zA-Z0-9\/\_]+');
+    Route::any('/apiservice/{moduleAndMethod}', function ($moduleAndMethod) {
+        return response()->json(\ModuleMicroHybrid\Api::resolveIncoming($moduleAndMethod, (json_decode(request()->getContent(), true) ?? [])))->setEncodingOptions(JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);;
+    })->where('moduleAndMethod', '[a-zA-Z0-9\/\_]+');
 });

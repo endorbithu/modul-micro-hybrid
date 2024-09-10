8e03beb88b9856afcd4201898851a1f2a774c9e2
Magyar Bertalan Márton <102574817+endorbithu@users.noreply.github.com>
2024-09-10 10:46:37 +0200

    Create ServiceProvider.php

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ModuleMicroHybrid\Providers;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+  
+    public function boot()
+    { 
+        if (!$this->app->routesAreCached()) {
+            include __DIR__ . '/../../routes/api.php';      
+        }   
+    }
+
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../../config/module-micro-hybrid.php' => config_path('module-micro-hybrid.php'),
+            ]);
+        }
+    }
+}
+
